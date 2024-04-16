a09d174a83a9194c2e79bb39680831d3dae961e5
Comport20 <130553274+Comport20@users.noreply.github.com>
2024-04-16 16:45:21 +0300

    test: Create test cases for Carlist

---
+++ src/Carlist.test.tsx
@@ -0,0 +1,35 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { describe, expect, test } from "vitest";
+import Carlist from "./components/Carlist";
+import { render, screen, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
+import userEvent from "@testing-library/user-event";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
+const wrapper = ({ children }: { children: React.ReactNode }) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
+
+describe("Carlist tests", () => {
+  test("component render", () => {
+    render(<Carlist />, { wrapper });
+    expect(screen.getByText(/Loading/i)).toBeInTheDocument();
+  });
+  test("Cars are fetched", async () => {
+    render(<Carlist />, { wrapper });
+    await waitFor(() => screen.getByText(/Add car/i));
+    expect(screen.getByText(/Ford/i)).toBeInTheDocument();
+  });
+  test("Open new car modal", async () => {
+    render(<Carlist />, { wrapper });
+    await waitFor(() => screen.getByText(/Add car/i));
+    await userEvent.click(screen.getByText(/Add car/i));
+    expect(screen.getByText(/Save/i)).toBeInTheDocument();
+  });
+});
